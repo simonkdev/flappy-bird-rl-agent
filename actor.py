9a85d60e29954f3ff16ed44ff0b516b6e5abccc2
simonkdev <simonkorten09@gmail.com>
2026-09-08 11:55:16 +0200

    basic actor module

---
+++ actor.py
@@ -0,0 +1,68 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+
+import config
+
+class Actor(tf.keras.Model):
+
+    def __init__(self, num_actions):
+        super().__init__()
+
+        self.cnn = tf.keras.Sequential([
+            layers.Conv2D(
+                config.CNN_FILTERS[0],
+                config.CNN_KERNEL_SIZES[0],
+                strides=config.CNN_STRIDES[0],
+                activation="relu"
+            ),
+            layers.Conv2D(
+                config.CNN_FILTERS[1],
+                config.CNN_KERNEL_SIZES[1],
+                strides=config.CNN_STRIDES[1],
+                activation="relu"
+            ),
+            layers.Conv2D(
+                config.CNN_FILTERS[2],
+                config.CNN_KERNEL_SIZES[2],
+                strides=config.CNN_STRIDES[2],
+                activation="relu"
+            ),
+            layers.Flatten(),
+        ])
+
+        self.fc = layers.Dense(
+            config.FC_UNITS,
+            activation="relu"
+        )
+
+        self.policy = layers.Dense(num_actions)
+
+        self.optimizer = tf.keras.optimizers.Adam(
+            learning_rate=config.LEARNING_RATE
+        )
+
+    def call(self, observation):
+        x = tf.cast(observation, tf.float32) / config.PIXEL_SCALE
+
+        x = self.cnn(x)
+        x = self.fc(x)
+
+        logits = self.policy(x)
+
+        return logits
+
+    def forward_prop(self, observation):
+        logits = self.call(observation)
+        probabilities = tf.nn.softmax(logits)
+
+        return probabilities
+
+    def update(self, loss, tape):
+        gradients = tape.gradient(
+            loss,
+            self.trainable_variables
+        )
+
+        self.optimizer.apply_gradients(
+            zip(gradients, self.trainable_variables)
+        )
