5c8f3e54857fb0e0140d78c9208ee70f970e4051
simonkdev <simonkorten09@gmail.com>
2026-09-08 11:54:49 +0200

    added python dependencies to devenv

--- devenv.nix
+++ devenv.nix
@@ -1,58 +1,43 @@
-{ pkgs, lib, config, inputs, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
 
 {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  languages.python = {
+    enable = true;
+    version = "3.13";
+    venv.enable = true;
+    venv.requirements = ''
+      tqdm
+    '';
+  };
 
-  # https://devenv.sh/packages/
-  packages = [
-    pkgs.git
-    pkgs.cmake
-    pkgs.wayland
-    pkgs.wayland-scanner
-    pkgs.libxkbcommon
-    pkgs.libffi
-    pkgs.glfw
-    pkgs.waylandpp
-    pkgs.gcc
+  packages = with pkgs; [
+    cmake
+    wayland
+    wayland-scanner
+    libxkbcommon
+    libffi
+    glfw
+    waylandpp
+    gcc
+    python313Packages.tensorflow
+    python313Packages.numpy
+    python313Packages.keras
+    python313Packages.tkinter
   ];
 
-  # https://devenv.sh/languages/
-  # languages.rust.enable = true;
   languages.cplusplus.enable = true;
 
   env.LD_LIBRARY_PATH = "${pkgs.wayland}/lib:${pkgs.libxkbcommon}/lib";
 
-  # https://devenv.sh/processes/
-  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
-
-  # https://devenv.sh/basics/
-  enterShell = ''
-    hello         # Run scripts directly
-    git --version # Use packages
-  '';
-
-  # https://devenv.sh/tasks/
   tasks = {
-     "flappyrl:libdemo".exec = "python3 lib/flappy-bird-env/demo.py --show-game-window --debug-window --infinite --real-time";
-   };
+    "flappyrl:libdemo".exec =
+      "python3 lib/flappy-bird-env/demo.py --show-game-window --debug-window --infinite --real-time";
+  };
 
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
 }
