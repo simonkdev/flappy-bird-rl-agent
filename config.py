776519f52a6c36080186230a7a25eb252e6d08c7
simonkdev <simonkorten09@gmail.com>
2026-09-08 11:55:28 +0200

    initial config

---
+++ config.py
@@ -0,0 +1,13 @@
+CNN_FILTERS = [32, 64, 64]
+CNN_KERNEL_SIZES = [8, 4, 3]
+CNN_STRIDES = [4, 2, 1]
+
+FC_UNITS = 256
+
+LEARNING_RATE = 3e-4
+
+PIXEL_SCALE = 255.0 # how many possible pixel values, since grayscale we have 256 possible values
+
+CRITIC_HIDDEN_UNITS = [256, 256, 256]
+
+CRITIC_LEARNING_RATE = 3e-4
